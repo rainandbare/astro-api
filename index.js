926ce638a653f08db99ec5d7ef0f81e16ffc2003
Suzette McCanny <suzettemccanny@gmail.com>
2019-11-16 14:37:08 -0500

    added basic routes

--- index.js
+++ index.js
@@ -2,16 +2,32 @@
 const express = require('express');
 // we create an app using express's express() function
 const app = express();
+const router = express.Router();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.json({"message": "Yes, It is working!"});
-});
+router.route('/')
+  .get((req, res) => {
+    res.json({ "message": "Yes! It's working!" });
+  });
 
-app.get('/new', (req, res) => {
-    res.json({"message": "a new route"});
-});
+router.route('/rat')
+  .get((req, res) => {
+    res.json({"message": "This is the rat page."});
+  });
+
+router.route('/rooster')
+  .get((req, res) => {
+    res.json({ "message": "This is the rooster page." });
+  });
+
+
+
+  //create a route where you enter your birthdate. it sends it as a string to the server - which converts it to a date
+
+  //for chinese horoscope the end point checks what dates you are inbetween and tells you what your animal is. 
+
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`I am listening on port ${port}`)
